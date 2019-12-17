7b07352dd53f434107b76d1582c4e574e106cc0d
Cooper Jones <cooper.jones@colorado.edu>
2019-12-17 15:06:12 -0700

    added secret config file

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+export default  function firebaseConfig(){
+return {
+    apiKey: "AIzaSyC2-ydIMLF0mAWAsY2YzYzh95B79mDEUho",
+    authDomain: "thetatau-etagamma-members.firebaseapp.com",
+    databaseURL: "https://thetatau-etagamma-members.firebaseio.com",
+    projectId: "thetatau-etagamma-members",
+    storageBucket: "thetatau-etagamma-members.appspot.com",
+    messagingSenderId: "320221233827",
+    appId: "1:320221233827:web:e350efd2eb66d4b75ab516",
+    measurementId: "G-0HH8DWH66W"
+    };
+}
